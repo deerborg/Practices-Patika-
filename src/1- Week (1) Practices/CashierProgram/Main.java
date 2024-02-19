039a8b58c187ee0644afc03ba285591333d2b310
Furkan Aydemir <152931069+deerborg@users.noreply.github.com>
2024-02-19 03:29:19 +0300

    Update Main.java
    add author

--- src/1- Week (1) Practices/CashierProgram/Main.java
+++ src/1- Week (1) Practices/CashierProgram/Main.java
@@ -2,8 +2,8 @@
 package CashierProgram;
 import java.util.Scanner;
 /**
- *
- * @author deer
+ * @author Furkan Aydemir, deerborg
+ * @since 2024
  */
 public class Main {
     public static void main(String[] args) {
